71c98d03b561d065adc4dce97400fd6be57f4df5
Papi Gringo <papigringo@Papis-Mac-mini.local>
2025-03-05 23:30:27 +0700

    Remove API routes for static export

--- next.config.js
+++ next.config.js
@@ -1,23 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  trailingSlash: true,
-  // This tells Next.js to export the app as static HTML/CSS/JS
   output: 'export',
-  // Images need to be unoptimized for static export
   images: {
     unoptimized: true,
   },
-  // Fix for yahoo-finance2 package
-  webpack: (config, { isServer }) => {
+  webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
       '../../tests/http/': false,
     };
-    
     return config;
+  },
+  // Disable app directory API routes
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: [],
   }
-  // Remove the rewrites section, it's not compatible with static export
 };
 
 module.exports = nextConfig;
