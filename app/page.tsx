a99a52877bce59f313935ded7179a9bfdbfe6e15
Papi Gringo <papigringo@Papis-Mac-mini.local>
2025-03-06 20:13:11 +0700

    Create minimal app directory for successful build

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,8 @@
-"use client"
-
-import dynamic from 'next/dynamic'
-import { Suspense, useEffect, useState } from 'react'
-
-const AssetAnalyzer = dynamic(() => import('../components/AssetAnalyzer.jsx'), {
-  ssr: false,
-  loading: () => <div>Loading...</div>
-})
-
 export default function Home() {
-  const [isMounted, setIsMounted] = useState(false)
-
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
-
-  if (!isMounted) {
-    return <div>Loading...</div>
+    return (
+      <main>
+        <h1>My Asset Analyzer</h1>
+        <p>Successfully deployed to Render!</p>
+      </main>
+    );
   }
-
-  return (
-    <main className="min-h-screen p-4">
-      <AssetAnalyzer />
-    </main>
-  )
-}
